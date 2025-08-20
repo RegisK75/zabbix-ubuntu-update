324110211ff2b78128119282f5e99cfb15ce034a
RegisK75 <regis.karakozian@gmail.com>
2025-08-20 08:26:31 +0200

    Create chekc-os-update.sh
    Linux shell script to check if Ubuntu OS update are available and if reboot is required

---
+++ chekc-os-update.sh
@@ -0,0 +1,21 @@
+## Shell to check Ubuntu OS update and if reboot is required.
+## Output is JSON file in /tmp folder.
+## This shell should be run via crontab on regular basis
+
+#!/bin/bash
+EXPORTFILE="/tmp/nc-os-update.json"
+if [ -f $EXPORTFILE ]; then
+	rm $EXPORTFILE
+fi
+echo -n "{\"reboot_required\":" > $EXPORTFILE
+if [ -f /var/run/reboot-required ]
+then
+        echo -n "1," >> $EXPORTFILE
+else
+	echo -n "0," >> $EXPORTFILE
+fi
+updates=`/usr/lib/update-notifier/apt-check 2>&1 | awk -F';' '{print $1}'`
+security_updates=`/usr/lib/update-notifier/apt-check 2>&1 | awk -F';' '{print $2}'`
+echo -n "\"update_available\":$updates," >> $EXPORTFILE
+echo -n "\"security_update_available\":$security_updates" >> $EXPORTFILE
+echo -n "}" >> $EXPORTFILE
